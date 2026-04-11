a9ac25fa597617941e21ff02c1e17a416b28b669
Vaibhavtripathi7 <vaibhavtripathi2020@outlook.com>
2026-04-11 20:40:48 +0530

    feat: add type safety for our jobapplications objects

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("holaaa")
+
+interface JobApplicationEvent { 
+    companyName: string;
+    role: string;
+    status: "Applied" | "Interviewing" | "Rejected"| "Offer" ;
+    workModel: "Remote"|"Onsite"| "Hybrid"; 
+    dateApplied: string;
+}
+
+const myFirstApplication: JobApplicationEvent = { 
+
+    companyName: "Google", 
+    role: "SDE Intern",
+    status: "Applied",
+    workModel: "Remote",
+    dateApplied: "something"
+};
+
+
