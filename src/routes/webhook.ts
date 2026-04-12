16dde75e52ccf7fb7540efb7a2fa9480f6a55244
Vaibhavtripathi7 <vaibhavtripathi2020@outlook.com>
2026-04-12 14:35:43 +0530

    feat: add a route for post request

---
+++ src/routes/webhook.ts
@@ -0,0 +1,45 @@
+import express from  'express';
+import type {Response, Request } from "express";
+
+interface JobApplicationEvent { 
+    companyName: string;
+    role: string;
+    status: "Applied" | "Interviewing" | "Rejected"| "Offer" ;
+    workModel: "Remote"|"Onsite"| "Hybrid"; 
+    dateApplied: string;
+}
+
+const myFirstApplication: JobApplicationEvent = { 
+
+    companyName: "Google", 
+    role: "SDE Intern",
+    status: "Applied",
+    workModel: "Remote",
+    dateApplied: "something"
+};
+
+function fetchLatestApplicationFromGmail(): Promise<JobApplicationEvent> { 
+    return new Promise((resolve, reject) => {
+        console.log("Fetching from servers");
+
+        setTimeout(()=> {
+            resolve(myFirstApplication);
+        }, 2000);
+    })
+} 
+
+const router_hook = express.Router();
+router_hook.post('/', async(req:Request, res:Response)=> {
+
+    try{
+        let results = await fetchLatestApplicationFromGmail();
+        console.log(`Company name: ${results.companyName} and status: ${results.status}`);
+        res.status(200).send("Webhook processed successfully");
+    }catch(error){
+        console.log(`Network error! ${error}`);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+
+export default router_hook;
