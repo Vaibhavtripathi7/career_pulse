f62306dfcd96a6cd70e20991645be222ef5dbbe6
Vaibhavtripathi7 <vaibhavtripathi2020@outlook.com>
2026-04-15 21:17:35 +0530

    feat: add google gmail api to fetch mails for specific mail id using oauth2

---
+++ src/services/gmail.ts
@@ -0,0 +1,31 @@
+import {google} from 'googleapis'
+import 'dotenv/config';
+
+const oauth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID as string,
+    process.env.CLIENT_SECRET as string,
+);
+oauth2Client.setCredentials({
+    refresh_token: process.env.GMAIL_REFRESH_TOKEN as string
+});
+
+
+const gmail = google.gmail({version: 'v1', auth: oauth2Client})
+
+async function fetchemails(){
+    let mail = await gmail.users.messages.list({userId: 'me', maxResults: 1})
+    const message = mail.data.messages;
+    if (!message || message.length === 0) {
+        console.log("no new emails")
+        return;
+    }
+    const firstmail = message[0]
+    if (!firstmail || !firstmail.id ) {
+        console.log("email exists")
+        return ;
+    }
+
+    let main_content = await gmail.users.messages.get({userId: 'me', id: firstmail.id})
+
+    console.log(main_content.data.snippet);
+}   
