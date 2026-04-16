c2a0bc05ff0d03894c9b59e088c06d71355048b3
Vaibhavtripathi7 <vaibhavtripathi2020@outlook.com>
2026-04-16 11:37:11 +0530

    feat: add route to fetch emails on endpoint access

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import router from './routes/health.js';
 import router_hook from "./routes/webhook.js";
-
+import router_for_mail from "./routes/sync.js";
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use('/api/health', router)
 app.use('/api/webhook', router_hook);
+app.use('/api/sync', router_for_mail);
 
 app.listen(PORT, ()=>{
     console.log("server is alive")
