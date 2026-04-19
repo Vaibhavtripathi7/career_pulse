61cd45b10e6ec589e34528f237dd1c91a9fe4cb1
Vaibhavtripathi7 <vaibhavtripathi2020@outlook.com>
2026-04-20 02:19:53 +0530

    fix: change from https to http, as server does not have SSL

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import type{ ApiResponse, Application } from "../types";
 
-const API_BASE_URL = 'https://localhost:3000/api';
+const API_BASE_URL = 'http://localhost:3000/api';
 
 export async function getApplication(): Promise<Application[]> {
 
