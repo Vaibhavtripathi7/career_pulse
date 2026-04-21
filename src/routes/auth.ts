6ab9111f21c7e8f08a99f63ebd9549c04e8cbe11
Vaibhavtripathi7 <vaibhavtripathi2020@outlook.com>
2026-04-21 19:15:17 +0530

    feat(auth): persist Google Oauth users and tokens in database

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,8 @@
 import { Router} from 'express';
 import type { Request, Response } from 'express';
-
+import prisma from '../db.js';
 import oauth2client from '../utils/googleclient.js';
-
-
+import { google } from 'googleapis';
 const  authrouter = Router();
 
 const scopes = [
@@ -29,6 +28,33 @@ authrouter.get('/google/callback', async (req: Request, res:Response)=> {
     }
     try {
         const { tokens } = await oauth2client.getToken(code);
+        oauth2client.setCredentials(tokens);
+
+        const oauth2 = google.oauth2({ auth: oauth2client, version: 'v2'});
+        const { data } = await oauth2.userinfo.get();
+        if (!data.email){
+            return res.status(400).send("No email provided by google");
+
+        }
+        const user = await prisma.user.upsert({
+            where: {email: data.email},
+            update: {
+                name: data.name ?? null,
+                googleId: data.id ?? null,
+                accessToken: tokens.access_token ?? null,
+                ...(tokens.refresh_token && { refreshToken: tokens.refresh_token})
+            },
+
+            create: {
+                email: data.email ,
+                name: data.name ?? null,
+                googleId: data.id ?? null,
+                accessToken: tokens.access_token ?? null,
+                refreshToken: tokens.refresh_token ?? null 
+            } 
+        });
+    
+
         console.log("success", tokens);
         res.send("Authentication successful!")
         
