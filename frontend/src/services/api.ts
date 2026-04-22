ff5c90138d541d9aa37b8dc3b0345067914b18eb
Vaibhavtripathi7 <vaibhavtripathi2020@outlook.com>
2026-04-22 21:34:05 +0530

    feat(api): configure Axios client with secure credential transmission

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,18 +1,40 @@
 import type{ ApiResponse, Application } from "../types";
+import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api';
+const api = axios.create({
+
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+    }
+});
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+
+        if (error.response && error.respone.status === 401) {
+
+            console.warn("Unauthorized! cookie is mising")
+
+        }
+        return Promise.reject(error);
+    }
+)
+
 
 export async function getApplication(): Promise<Application[]> {
 
     try {
-        const response = await fetch(`${API_BASE_URL}/applications`);
-        if (!response.ok){
-            throw new Error(`HTTP error! status: ${response.status}`);
+        const response = await api.get<ApiResponse>('/applications');
+
+        if (!response.data.success){
+            return response.data.data;
         }
-        const json: ApiResponse = await response.json();
-        if (json.success) {
-            return json.data;
-        }else {
+        else {
             throw new Error("API responeded with success: false");
         }
     } catch (error) {
@@ -20,3 +42,16 @@ export async function getApplication(): Promise<Application[]> {
         return [];
     }
 }
+
+export async function getUseProfile(){
+
+    try {
+        const response = await api.get('/auth/me');
+        return response.data;
+
+    } catch (error) {
+        return null;
+    }
+}
+
+export default api ;
