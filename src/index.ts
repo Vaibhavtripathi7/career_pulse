bb8623160f23d0a0446260215127598e6009492c
Vaibhavtripathi7 <vaibhavtripathi2020@outlook.com>
2026-04-22 23:49:24 +0530

    chore: remove unused commented code and clean up comments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import router from './routes/health.js';
-// import router_hook from "./routes/webhook.js";
 import router_for_mail from "./routes/sync.js";
 import router_db from "./routes/application.js";
 import cors from 'cors';
@@ -17,7 +16,6 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/health', router)
-// app.use('/api/webhook', router_hook);
 app.use('/api/sync', router_for_mail);
 app.use('/api/applications', router_db)
 app.use('/api/auth', authrouter);
