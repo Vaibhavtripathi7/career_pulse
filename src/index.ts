5f466b84d616419db9e1d19366184b13d753ef01
Vaibhavtripathi7 <vaibhavtripathi2020@outlook.com>
2026-04-22 23:28:25 +0530

    fix(api): add cors middleware with credentials parameter to bypass error while fetching cookies

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,12 @@ import cookieParser from "cookie-parser";
 const app = express();
 const PORT = 3000;
 
-app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true, 
+}));
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api/health', router)
 // app.use('/api/webhook', router_hook);
 app.use('/api/sync', router_for_mail);
