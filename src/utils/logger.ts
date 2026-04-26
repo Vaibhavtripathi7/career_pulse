f3d7fcf023479d4af9cf834f3e20ea15d0ca69a8
Vaibhavtripathi7 <vaibhavtripathi2020@outlook.com>
2026-04-26 23:19:41 +0530

    fix(logger): make pino logger environment-aware for production compatability

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,11 @@
 import pino from "pino";
 
-export const logger = pino({
+const isProd = process.env.NODE_ENV === "production";
+
+export const logger = isProd
+    ? pino()
+
+    : pino({
 
     level: "info",
     transport: {
